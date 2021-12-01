a036b669fcc6f31b3a60299353c78b243394cc85
Elodie Riou <elodie.riou@hotmail.com>
2021-12-01 10:33:26 +0100

    add function _putchar and _putstr

---
+++ _putstr.c
@@ -0,0 +1,17 @@
+#include "simpleshell.h"
+
+/**
+ * _putstr - function that print a string
+ * @s: a pointer to a string
+ * Return: the number of bytes of the string
+ */
+int _putstr(char *s)
+{
+	int i;
+
+	if (str == NULL)
+		return (-1);
+	for (i = 0; s[i]; i++)
+		_putchar(s[i]);
+	return (i);
+}
