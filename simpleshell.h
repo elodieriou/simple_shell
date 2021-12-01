99f4d39219204c9b0fa93509e095a81f63436d88
Elodie Riou <elodie.riou@hotmail.com>
2021-12-01 10:55:43 +0100

    update libraries

--- simpleshell.h
+++ simpleshell.h
@@ -2,6 +2,11 @@
 #define DEF_SIMPLESHELL
 
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int _putchar(char c);
 int _putstr(char *s);
